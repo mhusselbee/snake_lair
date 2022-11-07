46ec8ee0e7e9f35fdb544a70a9d45fc025a9b222
mhusselbee <mhusselbee@lifespark.com>
2022-11-07 15:16:24 -0800

    Notes

--- README.md
+++ README.md
@@ -1,3 +1,15 @@
 # snake_lair
 
 A playground for python.
+
+## Setup & Usage
+
+```bash
+# Install libraries
+pip install requirements.txt
+
+# Run the mouse wiggler python script
+python3 wiggle.py
+
+```
+
--- wiggle.py
+++ wiggle.py
@@ -1,10 +1,9 @@
-import pyautogui as pag
+import pyautogui # Note that pyautogui will prompt for accessibility permissions
 import random
 import time
-
 
 while True:
     x = random.randint(600, 700)
     y = random.randint(200, 700)
-    pag.moveTo(x, y, 0.5)
+    pyautogui.moveTo(x, y, 0.5)
     time.sleep(2)
